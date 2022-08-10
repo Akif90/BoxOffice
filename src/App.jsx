8ad05cf9255245796c66c6b3b29ed8a95a752f8d
Akif Ahmed <akifahmed90@gmail.com>
2022-08-10 11:34:56 +0530

    Added dummy routes

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,22 @@
 import React from 'react';
+import { Switch, Route } from 'react-router-dom';
 
 function App() {
-  return <div>Hello</div>;
+  return (
+    <Switch>
+      <Route exact path="/">
+        this is home page
+      </Route>
+      {/* we are using exact so that react routes exactlty acc to the path specified */}
+      {/* we could use exact={true} or just exact in react  */}
+      <Route exact path="/now">
+        OH you are here now
+      </Route>
+      {/* The last route works only when the all the other fails to work
+      so it is basically used for rendering error message page like 404 not found */}
+      <Route>Not Found</Route>
+    </Switch>
+  );
 }
 
 export default App;
